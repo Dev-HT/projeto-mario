7760c3b49e61fbf42fcd726873c299057e98fae4
Dev-HT <hse.sm.7499@gmail.com>
2023-03-29 22:33:27 -0300

    added js language

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+
+
+const modal = document.querySelector(".modal");
+
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+
+const video = document.getElementById("video");
+
+
+const linkDoVideo = video.src;
+
+function alternarModal(){
+     
+    modal.classList.toggle("aberto"); 
+}
+
+
+botaoTrailer.addEventListener("click", () => {
+    
+    alternarModal();   
+    video.setAttribute("src", "https://www.youtube.com/embed/Cb4WV4aXCpk);   
+});
+
+
+botaoFecharModal.addEventListener("click", () => {
+   
+    alternarModal(); 
+    
+    video.setAttribute("src", "https://www.youtube.com/embed/Cb4WV4aXCpk");
+});
